90fbf3e4dd7d8ec6d01796f57fc09f90deabdf23
Wang Mengyuan <2873054939@qq.com>
2020-10-14 11:00:29 +0800

    fix

--- python_codes/ccnu/ccun.py
+++ python_codes/ccnu/ccun.py
@@ -1,23 +1,38 @@
 import requests
+import json
+import pandas as pd
 from bs4 import BeautifulSoup
-payload = {"input[pageNo]": "0", "input[pageSize]": "9", "input[sort][beginsName]": "asc",
-           "input[mustWildcardFilter][teacherName-keyword]": "",
-           "input[mustWildcardFilter][researchDirection-keyword]": "", "input[mustFilter][resumeType-keyword][]": "CH"}
-headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-           'Accept-Encoding': 'gzip, deflate',
-           'Accept-Language': 'zh-CN,zh;q=0.9',
-           'Cache-Control': 'max-age=0',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
-           'Connection': 'keep-alive',
-           'Host': 'faculty.ccnu.edu.cn',
-           'Upgrade-Insecure-Requests': '1'}
-url = 'http://grzy.cug.edu.cn/KaiCao/zh_CN/index.htm'
-#http://grzy.cug.edu.cn/KaiCao/zh_CN/index.htm
-req = requests.get(url = url,data=payload,headers = headers)
-
-req.encoding = 'utf-8'
-req.text
+data = {
+	"input[pageNo]": "1",
+	"input[pageSize]": "300",
+	"input[sort][beginsName]": "asc",
+	"input[mustWildcardFilter][teacherName-keyword]": "",
+	"input[mustWildcardFilter][researchDirection-keyword]": "",
+	"input[mustFilter][resumeType-keyword][]": "CH"
+}
+headers = {'Accept':'application/json, text/plain, */*',
+           'Accept-Encoding':'gzip, deflate',
+           'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+           'Cache-Control':'max-age=0',
+           'Connection':'keep-alive',
+           'Content-Length':'250',
+           'Content-Type':'application/x-www-form-urlencoded',
+           'Host':'faculty2.ccnu.edu.cn',
+           'Origin':'http://faculty.ccnu.edu.cn',
+           'Referer':'http://faculty.ccnu.edu.cn/ch/index',
+           'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'}
+url = 'http://faculty2.ccnu.edu.cn/resume/searchResume?random=0.6490731693679087'
+req = requests.post(url = url,data=data,headers = headers)
 req.status_code
+req.encoding = 'utf-8'
+
+listDatas = json.loads(req.text)["listDatas"]
+teacherinfolist = []
+for i in range(len(json.loads(req.text)["listDatas"])):
+    teacherinfolist.append({"姓名":listDatas[i]['_source']['nameChinese'],"工号":listDatas[i]['_source']['jobCode'],"学院":listDatas[i]['_source']['college'],"出生年月":listDatas[i]['_source']['birthDate'],"专业职称":listDatas[i]['_source']['jobTitle'],"教育背景":listDatas[i]['_source']['educationBackground']})
+
+pd.DataFrame(teacherinfolist)[["姓名","工号","学院","出生年月","专业职称","教育背景"]].to_excel('./ccun_teacherinfolist.xlsx')
+
 
 
 
